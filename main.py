c400364a6cfb72acdccb8e30f2047107111ff323
DaniilGavrin <bksbank@bk.ru>
2024-01-19 19:07:57 +0300

    main update

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import
+# Импортируем telebot + requests
+import telebot
+import requests
+from message import *
+
+# НАчальный код для работы с ботом
+bot = telebot.TeleBot("6786465313:AAGWvU4ppWSAmP37BKEXVfW63hKWqycjzQk")
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    send_welcome_message(bot, message)
+    
+bot.polling()
---
+++ message.py
@@ -0,0 +1,2 @@
+def send_welcome_message(bot, message):
+    bot.send_message(message.chat.id, 'Привет! Я')
