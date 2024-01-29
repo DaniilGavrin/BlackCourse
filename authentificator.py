efc107fffe6054f34985eab4bd870764ff2a88e0
DaniilGavrin <bksbank@bk.ru>
2024-01-29 20:32:32 +0300

    auth update

--- authentificator.py
+++ authentificator.py
@@ -12,32 +12,22 @@ class Authentificator:
             database="bottg"
         )
         self.cursor = self.db.cursor()
-        # проверяем подключение к базе данных
-        try:
-            self.cursor.execute("SELECT * FROM users")
-            self.db.commit()
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
     def login(self, bot, message):
         # код авторизации
-        bot.send_message(message.chat.id, "Введите логин!")
+        login = bot.send_message(message.chat.id, "Введите логин!")
+        bot.register_next_step_handler(login, self.get_login)
 
+    def get_login(self, bot, message):
+        # код авторизации
         login = message.text
         print(login)
         self.cursor.execute("SELECT * FROM users WHERE login = %s", (login,))
-        print(self.cursor.rowcount())
-
-        if cursor.rowcount() > 0:
-            msg = bot.send_message(message.chat.id, "Введите пароль!")
-        elif cursor.rowcount() == 0:
-            msg = bot.send_message(message.chat.id, "Пользователь с таким логином не существует!")
+        result = self.cursor.fetchone()
+        if result is None:
+            bot.send_message(message.chat.id, "Такого пользователя не существует!")
         else:
-            msg = bot.send_message(message.chat.id, "Ошибка!")
+            bot.send_message(message.chat.id, "Введите пароль!")
+            bot.register_next_step_handler(message, self.get_password)
 
     def register(self, bot, message):
         # код регистрации
