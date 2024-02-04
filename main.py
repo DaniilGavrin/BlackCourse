8652af17bc2594feec140b7efe007f9e0f500525
DaniilGavrin <bksbank@bk.ru>
2024-02-04 21:32:12 +0300

    GLOBAL UPDATE FIX FROM AIOGRAM

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Импортируем telebot + requests
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import logging
 import requests
 from message import *
 from license import *
@@ -6,74 +9,15 @@ from authentificator import *
 import mysql.connector
 from mysql.connector import errorcode
 
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token="6786465313:AAGWvU4ppWSAmP37BKEXVfW63hKWqycjzQk")
+
+dp = Dispatcher(bot, storage=MemoryStorage())
+
 class Bot:
     def __init__(self):
-        # подключение к базе данных
-        db = mysql.connector.connect(
-            host="172.17.0.2",
-            user="root",
-            password="090807",
-            database="bottg"
-        )
-        cursor = db.cursor()
-        # проверяем подключение к базе данных
-        try:
-            cursor.execute("SELECT * FROM users")
-            db.commit()
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
-        # Инициализация бота
-        self.bot = telebot.TeleBot("6786465313:AAGWvU4ppWSAmP37BKEXVfW63hKWqycjzQk")
-
-        # Регистрация обработчиков сообщений
-        self.register_handlers()
-
-    def run(self):
-        # запуск бота
-        self.bot.polling()
-
-    def register_handlers(self):
-        message_handler = MessageHandler()
-        license_handler = LicenseHandler()
-        authentificator = Authentificator()  # Добавить инициализацию объекта authentificator
-
-        @self.bot.message_handler(commands=['start'])
-        def start_message(message):
-            message_handler.send_welcome_message(self.bot, message)
-
-        @self.bot.message_handler(commands=['help'])
-        def help_message(message):
-            message_handler.send_help_message(self.bot, message)
-
-        @self.bot.message_handler(commands=['license'])
-        def give_license(message):
-            license_handler.license(self.bot, message)
-
-        @self.bot.callback_query_handler(func=lambda call: True)
-        def callback_query(call):
-            try:
-                if call.data == "login":
-                    authentificator.login(self.bot, call.message)
-                elif call.data == "register":
-                    authentificator.register(self.bot, call.message)
-                elif call.data == "no_register":
-                    authentificator.no_register(self.bot, call.message)
-                elif call.data == "license":
-                    license_handler.license(self.bot, call)
-                elif call.data == "message":
-                    message_handler.message(self.bot, call)
-            except Exception as e:
-                print(e)
-            
-
-# Создаем экземпляр класса Bot
-bot = Bot()
-
-# Запуск бота
-bot.run()
+        self.dp = dp
+    
+    async def start(self, message: types.Message):
+        MessageHandler.send_welcome_message(self, message: types.Message)
--- message.py
+++ message.py
@@ -2,16 +2,11 @@
 from authentificator import *
 
 class MessageHandler:
-    @staticmethod
-    def send_welcome_message(bot, message):
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        login = types.InlineKeyboardButton(text='Войти', callback_data='login')
-        register = types.InlineKeyboardButton(text='Зарегистрироваться', callback_data='register')
-        markup.add(login, register)
-        bot.send_message(message.chat.id, 'Привет! Это я бот BlackCourse!  \nТвой надежный помощник в мир новых и интересных знаний! \nС помощью нашего бота ты сможешь: \n•Получить те знания которые ты так хотел, но не мог ранее.\n•Получить все необходимые навыки.\n•Пройти весь путь от А до Я с нашим инновационным подходом.\n•Персонализированное обучение.\nШирокий выбор курсов для обучения.\n•Гибкость и доступность.\n•Мониторинг прогресса и достижений.\n•Система рекомендаций в соответствии с вашими успехами.\n•Удобный доступ.\n•Мгновенная обратная связь.\n•Система наград и мотиваций.\n•Интерактивное обучение.\n•Мультиязычность.\n•Бот доступен во всех странах!', reply_markup=markup)
+    async def send_welcome_message(self, message: types.Message):
+        await message.answer('Привет! Это я бот BlackCourse!  \nТвой надежный помощник в мир новых и интересных знаний! \nС помощью нашего бота ты сможешь: \n•Получить те знания которые ты так хотел, но не мог ранее.\n•Получить все необходимые навыки.\n•Пройти весь путь от А до Я с нашим инновационным подходом.\n•Персонализированное обучение.\nШирокий выбор курсов для обучения.\n•Гибкость и доступность.\n•Мониторинг прогресса и достижений.\n•Система рекомендаций в соответствии с вашими успехами.\n•Удобный доступ.\n•Мгновенная обратная связь.\n•Система наград и мотиваций.\n•Интерактивное обучение.\n•Мультиязычность.\n•Бот доступен во всех странах!')
 
     def send_help_message(self, bot, message):  # Исправить имя функции здесь
-        bot.send_message(message.chat.id, 'Помощь')
+        pass
 
     def no_register(self, bot, message):
-        bot.send_message(message.chat.id, "Хорошо, до скорой встречи!")
+        pass
